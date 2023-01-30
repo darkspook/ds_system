04add9c23e567c32f205f5d506a1b59889e9491d
darkspook <jcba003@gmail.com>
2023-01-30 06:55:43 +0000

    removal of ict_inventory dir

--- ict_inventory/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Asset, Component, EndUser, Brand, Location, Type
-
-admin.site.register(Asset)
-admin.site.register(Component)
-admin.site.register(EndUser)
-admin.site.register(Brand)
-admin.site.register(Location)
-admin.site.register(Type)
--- ict_inventory/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class IctInventoryConfig(AppConfig):
-	name = 'ict_inventory'
-	verbose_name = 'ICT Inventory'
